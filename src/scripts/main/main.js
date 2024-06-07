cc189faed202ae94ad6e1941dba8f003d478fa1a
alexeev <a.alexeev@ilsi.pro>
2024-06-07 12:58:09 +0300

    feat: add scrollToTopBtn

--- src/scripts/main/main.js
+++ src/scripts/main/main.js
@@ -1,1 +1,31 @@
-// alert('main script!')
+function throttle(func, delay) {
+    let lastCall = 0;
+    return function (...args) {
+        const now = new Date().getTime();
+        if (now - lastCall < delay) {
+            return;
+        }
+        lastCall = now;
+        return func(...args);
+    };
+}
+
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+function handleScroll() {
+
+    if (window.scrollY > 300) {
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+}
+
+window.addEventListener('scroll', throttle(handleScroll, 200));
+
+scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
