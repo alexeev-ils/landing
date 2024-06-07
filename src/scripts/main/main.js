5c8253f485de961e37980b99709c99bff9d14bca
alexeev <a.alexeev@ilsi.pro>
2024-06-07 14:25:31 +0300

    feat: add accordion for tariffs

--- src/scripts/main/main.js
+++ src/scripts/main/main.js
@@ -29,3 +29,30 @@ scrollToTopBtn.addEventListener('click', () => {
         behavior: 'smooth'
     });
 });
+
+
+function accordion() {
+    const accordions = document.querySelectorAll('.tariff__accordion');
+    accordions.forEach((acco) => {
+        const head = acco.querySelector('.accordion__head');
+        const body = acco.querySelector('.accordion__body');
+
+        let isOpen = acco.classList.contains('open')
+
+        head.addEventListener('click', () => {
+            if (isOpen) {
+                acco.classList.remove('open');
+                body.style.maxHeight = 0 + "px";
+
+                isOpen = !isOpen;
+            } else {
+                acco.classList.add('open');
+                body.style.maxHeight = body.scrollHeight + "px";
+                isOpen = !isOpen;
+            }
+        })
+
+    })
+}
+
+accordion();
